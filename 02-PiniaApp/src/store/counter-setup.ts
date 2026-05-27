6ab4fa4a470f5a9d9c8f366b109ae60dcdfd2169
diazjohan98 <vasquez.johan@correounivalle.edu.co>
2026-05-27 15:53:47 -0500

    counter con setup

---
+++ 02-PiniaApp/src/store/counter-setup.ts
@@ -0,0 +1,25 @@
+import { defineStore } from "pinia";
+import { computed, ref } from "vue";
+
+export const useCounterSetupStore = defineStore('counterSetup',() => {
+    const count = ref<number>(0);
+    const lastChange = ref<Date>();
+
+
+    const incrementBy = ( value: number ) => {
+        count.value += value;
+        lastChange.value = new Date();
+    };
+
+    return { 
+        //state propierties
+        count, 
+        lastChange,
+        //Getters
+        squareCount: computed( () => count.value * count.value ),
+        
+        //Actions
+        incrementBy, 
+        increment: () => incrementBy(1),
+    };
+});
