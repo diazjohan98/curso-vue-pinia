efa31fdaf01da3576f5b780a3527c5c287228504
diazjohan98 <vasquez.johan@correounivalle.edu.co>
2026-05-27 15:42:29 -0500

    utilizando pinia:  haciendo counter con pinia

---
+++ 02-PiniaApp/src/counter/components/CounterOptionsValue.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useCounterOptionsStore } from '@/store/counter.options';
+
+
+
+const counter = useCounterOptionsStore()
+
+</script>
+
+<template>
+  <h1>El valor del {{ counter.count }}</h1>
+  <h4>Ultimo cambio: {{ counter.lastChange }}</h4>
+</template>
+
+<style scoped>
+
+</style>
--- 02-PiniaApp/src/counter/pages/Counter1Page.vue
+++ 02-PiniaApp/src/counter/pages/Counter1Page.vue
@@ -1,17 +1,32 @@
 <script setup lang="ts">
 import { useCounterOptionsStore } from '@/store/counter.options.ts' 
 import { storeToRefs } from 'pinia';
+import CounterOptionsValue from '../components/CounterOptionsValue.vue';
 
   const counterStore = useCounterOptionsStore();
   const { count, squareCount} = storeToRefs( counterStore )
+  const { increment, incrementBy } = counterStore;
+  const resetStore = () => counterStore.$reset();
 </script>
 <template>
   <h1>Pinia options</h1>
   <h4>Counter: {{ count }}</h4>
   <h4>Square: {{ squareCount }}</h4>
   
-  
+  <br />
+  <button @click="increment()">+1</button>
+  <button @click="incrementBy(2)">+2</button>
+  <button @click="incrementBy(5)">+5</button>
+  <button @click="resetStore()">Reset</button>
+
+  <br>
+  <CounterOptionsValue />
   
 </template>
 
-<style scoped></style>
+<style scoped>
+button {
+  margin-left: 5px;
+}
+
+</style>
