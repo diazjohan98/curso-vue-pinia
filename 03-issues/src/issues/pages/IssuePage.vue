56dce1d79906ad4e8256a2f72bb9c4d00be3440c
diazjohan98 <vasquez.johan@correounivalle.edu.co>
2026-06-03 17:16:37 -0500

    mostrar issues de forma individual

--- 03-issues/src/issues/pages/IssuePage.vue
+++ 03-issues/src/issues/pages/IssuePage.vue
@@ -1,9 +1,30 @@
 <script setup lang="ts">
+import LoaderSpinner from 'src/shared/components/LoaderSpinner.vue';
+import { useRoute } from 'vue-router';
+import IssueCard from '../components/issues-list/IssueCard.vue';
+
+
+const route = useRoute();
+const { id } = route.params;
 
 </script>
 
 <template>
-    <span class="text-h2">Issue: </span>
+  <LoaderSpinner color="white" />
+
+  <router-link class="text-white" to="/">Go back</router-link>
+
+  <!-- Header -->
+  <IssueCard />
+
+  <!-- Comentarios  -->
+  <LoaderSpinner :thickness="1" size="1.5rem" :show-text="false" />
+
+  <div class="column">
+    <span class="text-h4 q-mb-md">Comments (5)</span>
+    <IssueCard v-for="comment in 5" :key="comment" />
+  </div>
+
 </template>
 
 <style scoped></style>
